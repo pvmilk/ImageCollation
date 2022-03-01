4f63b0503dafa3f2d0fe5ab003bad48624618563
Milk Phongtharin <pvmilk@gmail.com>
2022-03-01 23:27:16 +0900

    make the file more easy to change weight and model

--- IllustrationMatcher/utils/models.py
+++ IllustrationMatcher/utils/models.py
@@ -1,44 +1,48 @@
+import enum
 import os
 import torch
 from torch import nn
 
 script_path = os.path.abspath(__file__)
 
-copyweight = False
-model_name = 'resnet18ft1'
 
-resnet_50 = torch.hub.load('pytorch/vision:v0.6.0', 'resnet50', pretrained=True)
+class WeightEnum(str, enum.Enum):
+    RESNET18 = 'resnet18'
+    RESNET50 = 'resnet50'
+    RESNET18FT0 = 'resnet18ft0'
+    RESNET18FT1 = 'resnet18ft1'
+    RESNET50FT1 = 'resnet50ft1'
 
-if copyweight:
-    path_model = {
-        'resnet18ft0' :
-        ('weights/simsiam_20220218-014917/checkpoints/last.pt', 'resnet18'),
-        'resnet18ft1' :
-        ('weights/simsiam_20220224-160133/checkpoints/last.pt', 'resnet18'),
-        'resnet50ft1' :
-        ('weights/simsiam_20220224-154343/checkpoints/last.pt', 'resnet50'),
-    }
-
-    model_path = os.path.join(
-        os.path.dirname(script_path),
-        path_model[model_name][0]
-    )
-    resnet_50 = torch.hub.load(
-        'pytorch/vision:v0.6.0',
-        path_model[model_name][1],
-        pretrained=True
-    )
+    def __str__(self):
+        return self.value
 
 
-class ResNet50Conv4(nn.Module):
-    def __init__(self, original_model):
-        super(ResNet50Conv4, self).__init__()
+weight2tuple = {
+    'resnet18' : (None, 'resnet18'),
+    'resnet50' : (None, 'resnet50'),
+    'resnet18ft0' :
+    ('weights/simsiam_20220218-014917/checkpoints/last.pt', 'resnet18'),
+    'resnet18ft1' :
+    ('weights/simsiam_20220224-160133/checkpoints/last.pt', 'resnet18'),
+    'resnet50ft1' :
+    ('weights/simsiam_20220224-154343/checkpoints/last.pt', 'resnet50'),
+}
+
+
+class ResNetConv4(nn.Module):
+    def __init__(self, original_model, path2weight=None):
+        super(ResNetConv4, self).__init__()
         self.features = nn.Sequential(*list(original_model.children())[:-3])
 
-        if copyweight:
-            features_weights = self.features.state_dict()
+        if path2weight is not None:
 
+            model_path = os.path.join(
+                os.path.dirname(script_path),
+                path2weight
+            )
             model_weights = torch.load(model_path)
+
+            features_weights = self.features.state_dict()
 
             # print(self.features.state_dict()['0.weight'][0, 0, ...])
             # print(model_weights['backbone.0.weight'][0, 0, ...])
@@ -59,6 +63,16 @@ class ResNet50Conv4(nn.Module):
         return x
 
 
-def get_conv4_model():
-    conv4 = ResNet50Conv4(resnet_50)
+def get_conv4_model(weight: WeightEnum = 'resnet50'):
+    orig_model = torch.hub.load(
+        'pytorch/vision:v0.6.0',
+        weight2tuple[weight][1],
+        pretrained=True
+    )
+
+    conv4 = ResNetConv4(orig_model, path2weight=weight2tuple[weight][0])
     return conv4
+
+
+if __name__ == "__main__":
+    get_conv4_model('resnet50')
